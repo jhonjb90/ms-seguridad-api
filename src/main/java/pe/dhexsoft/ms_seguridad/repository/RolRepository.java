c56048778143aa0dc61f9b403102e75a0beb5f8b
Jhon Inofuente <jhonjb90@gmail.com>
2024-12-08 17:03:16 -0500

    terminando de implementar el servicio autheticacion

--- src/main/java/pe/dhexsoft/ms_seguridad/repository/RolRepository.java
+++ src/main/java/pe/dhexsoft/ms_seguridad/repository/RolRepository.java
@@ -3,5 +3,9 @@ package pe.dhexsoft.ms_seguridad.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pe.dhexsoft.ms_seguridad.entity.Rol;
 
+import java.util.Optional;
+
 public interface RolRepository extends JpaRepository<Rol, Long> {
+
+    Optional<Rol> findByNombreRol(String rol);
 }
--- src/main/java/pe/dhexsoft/ms_seguridad/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/pe/dhexsoft/ms_seguridad/service/impl/AuthenticationServiceImpl.java
@@ -1,12 +1,16 @@
 package pe.dhexsoft.ms_seguridad.service.impl;
 
 import org.springframework.stereotype.Service;
+import pe.dhexsoft.ms_seguridad.aggregates.constants.Constants;
 import pe.dhexsoft.ms_seguridad.aggregates.request.SingUpRequest;
+import pe.dhexsoft.ms_seguridad.entity.Rol;
+import pe.dhexsoft.ms_seguridad.entity.Role;
 import pe.dhexsoft.ms_seguridad.entity.Usuario;
 import pe.dhexsoft.ms_seguridad.repository.RolRepository;
 import pe.dhexsoft.ms_seguridad.repository.UsuarioRepository;
 import pe.dhexsoft.ms_seguridad.service.AuthenticationService;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -24,19 +28,43 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     //-----------------------metodos---------------------------------
     @Override
     public Usuario singUpRequest(SingUpRequest signUpRequest) {
-        return null;
+        Usuario usuario = getUsuarioEntity(signUpRequest);
+        usuario.setRoles(Collections.singleton(getRoles(Role.USER)));
+        return usuarioRepository.save(usuario);
     }
 
     @Override
     public Usuario singUpAdmin(SingUpRequest signUpRequest) {
-        return null;
+        Usuario usuario = getUsuarioEntity(signUpRequest);
+        usuario.setRoles(Collections.singleton(getRoles(Role.ADMIN)));
+        return usuarioRepository.save(usuario);
     }
 
     @Override
     public List<Usuario> todos() {
-        return List.of();
+        return usuarioRepository.findAll();
     }
 
 
     //-----------------------metodos de apoyo ---------------------------------
+    private Usuario getUsuarioEntity(SingUpRequest singUpRequest){
+        return Usuario.builder()
+                .nombres(singUpRequest.getNombres())
+                .apellidos(singUpRequest.getApellidos())
+                .email(singUpRequest.getEmail())
+                .password(singUpRequest.getPassword())
+                .tipoDoc(singUpRequest.getTipoDoc())
+                .numDoc(singUpRequest.getNumDoc())
+                .isAccountNonExpired(Constants.STATUS_ACTIVE)
+                .isAccountNonLocked(Constants.STATUS_ACTIVE)
+                .isCredentialNonExpired(Constants.STATUS_ACTIVE)
+                .isEnabled(Constants.STATUS_ACTIVE)
+                .build();
+    }
+
+    private Rol getRoles(Role rolBuscado){
+        return rolRepository.findByNombreRol(rolBuscado.name())
+                .orElseThrow(
+                        () -> new RuntimeException("ERROR EL ROL NO EXISTE:" + rolBuscado.name()));
+    }
 }
